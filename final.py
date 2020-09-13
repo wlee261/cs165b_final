f5709e70a57578e0090ffdd665c91a5c999b6077
wlee261 <38049741+wlee261@users.noreply.github.com>
2020-09-12 21:22:57 -0700

    Add files via upload

--- final.py
+++ final.py
@@ -1,11 +1,19 @@
 import cv2
 import numpy as np
 import random
-import matplotlib
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import glob
+import os
 from sklearn.cluster import KMeans, MiniBatchKMeans
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
+import pandas as pd
+import time
 
 class Visual_BOW():
     def __init__(self, k=20, dictionary_size=50):
@@ -27,6 +35,56 @@ class Visual_BOW():
             test_features: list/array of size n_images_test x k x feature_dim
             test_labels: list/array of size n_images_test
         '''
+
+
+        #os.system(pip3 install --user pandas)
+        
+        FILEPATH = "/Users/graceshin/Downloads/101_ObjectCategories/*"
+        
+        filenames = []
+        images = []
+        train_features = []
+        train_labels = []
+        test_features = []
+        test_labels = []
+        rand = 0
+        filenames = glob.glob(FILEPATH)
+        
+        for i in range(len(filenames)):
+            images.append([])
+            images[i] = glob.glob(filenames[i]+"/*")
+            images[i].append(filenames[i])
+
+       
+        #print(len(descs[0]))
+        for x in range(len(images)):
+            for y in range(len(images[x])-1):
+                rand = random.randint(0,100)
+                print(images[x][y])
+                img1 = cv2.imread(images[x][y])
+                gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+                sift = cv2.xfeatures2d.SIFT_create()
+                (kp, descs) = sift.detectAndCompute(gray, None)
+
+                if descs is None:
+                    continue
+                if (len(descs)>self.k):
+                    descs = descs[0:self.k]
+                    
+                (head, tail) = os.path.split(images[x][len(images[x])-1])
+                if(rand > 70):
+                    test_features.append(descs)
+                    test_labels.append(tail)
+                else:
+                    train_features.append(descs)
+                    train_labels.append(tail)
+                    
+        '''print(len(train_features))
+        print(len(train_features[0]))
+        print(len(train_features[0][0]))
+        print(train_labels)
+        print(np.shape(train_features))'''
+        
         return train_features, train_labels, test_features, test_labels
 
     def create_dictionary(self, features):
@@ -42,6 +100,14 @@ class Visual_BOW():
         Output:
             kmeans: trained k-means object (algorithm trained on the flattened feature list)
         '''
+
+        flattened = []
+        for i in range(len(features)):
+            for x in range(len(features[i])):
+                flattened.append(features[i][x])
+        kmeans = MiniBatchKMeans(n_clusters = self.dictionary_size).fit(flattened)
+        #print(kmeans.labels_)
+        #print(np.shape(flattened))
         return kmeans
 
     def convert_features_using_dictionary(self, kmeans, features):
@@ -57,6 +123,34 @@ class Visual_BOW():
         Output:
             features_new: list/array of size n_images x dictionary_size
         '''
+        clusters = kmeans.cluster_centers_
+        #kmean_labels = []
+        #kmean_labels = kmeans.labels_.tolist()
+        prediction = []
+        features_new = []
+        counter = 0
+        
+        for z in range(len(features)):
+            features_new.append([])
+            for i in range(self.dictionary_size):
+                features_new[z].append(0)
+
+
+        flattened = []
+        for a in range(len(features)):
+            for b in range(len(features[a])):
+                flattened.append(features[a][b])
+
+        prediction = kmeans.predict(flattened)
+        for x in range(len(features)):
+            for y in range(len(features[x])):
+                features_new[x][prediction[counter]] = features_new[x][prediction[counter]] + 1
+                counter = counter+1
+
+                
+        #print(features_new)
+        #print(np.shape(features))
+        #print(len(kmean_labels))
         return features_new
 
     def train_svm(self, inputs, labels):
@@ -70,7 +164,11 @@ class Visual_BOW():
         Output:
             clf: trained svm classifier object (algorithm trained on the inputs/labels data)
         '''
-        return clf
+        clf = svm.SVC()
+        clf.fit(inputs, labels)
+        return clf            
+
+
 
     def test_svm(self, clf, inputs, labels):
         '''
@@ -84,6 +182,22 @@ class Visual_BOW():
         Output:
             accuracy: percent of correctly predicted samples
         '''
+
+        correct = 0
+        total = 0
+        changedim = []
+        for x in range(len(inputs)):
+            changedim.append([])
+            changedim[x].append(inputs[x])
+
+        
+        for i in range(len(changedim)):
+            if(clf.predict(changedim[i]) == labels[i]):
+                correct = correct + 1
+            total = total + 1
+
+        accuracy = correct/total
+        print(accuracy)
         return accuracy
 
     def save_plot(self, features, labels):
@@ -97,6 +211,41 @@ class Visual_BOW():
             features: new features (converted using the dictionary) of size n_images x dictionary_size
             labels: list/array of size n_images
         '''
+
+        SAVEPATH = '/Users/graceshin/Downloads/plot.png'
+
+        N = len(labels)
+        
+        principal_axes = []
+        reduced_data = []
+        pca = PCA(n_components = 2)
+        pca.fit(features)
+        principal_axes = pca.components_
+        principal_axes = np.transpose(principal_axes)
+        reduced_data = np.dot(features, principal_axes)
+
+        mydict = {}
+        i = 0
+        for item in labels:
+            if(i>0 and item in mydict):
+                continue
+            else:
+                i = i+1
+                mydict[item] = i
+
+        k = []
+        for item in labels:
+            k.append(mydict[item])
+
+        colors = []
+        for i in range(N):
+            colors.append('#%06X' %random.randint(0, 0xFFFFFF))
+
+        plt.scatter(reduced_data[:,0], reduced_data[:,1], c = k, s = 5, cmap = mpl.colors.ListedColormap(colors))
+        plt.savefig(SAVEPATH)
+        
+        
+        
 
 ############################################################################
 ################## DO NOT MODIFY ANYTHING BELOW THIS LINE ##################
@@ -118,6 +267,8 @@ class Visual_BOW():
         return accuracy
 
 if __name__ == "__main__":
+    start_time = time.time()
     alg = Visual_BOW(k=20, dictionary_size=50)
     accuracy = alg.algorithm()
     print("Final accuracy of the model is:", accuracy)
+    print("--- %s seconds ---" % (time.time() - start_time))
